d109b1569e0b451200805dd092b04167ab1ca578
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-20 00:42:24 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,35 +1,45 @@
 def heapify(data, n, i, swaps):
-    smallest = i 
-    left = 2 * i + 1   
-    right = 2 * i + 2     
-    if left < n and data[i] > data[left]:
+    smallest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and data[left] < data[smallest]:
         smallest = left
-    if right < n and data[smallest] > data[right]:
+
+    if right < n and data[right] < data[smallest]:
         smallest = right
+
     if smallest != i:
         data[i], data[smallest] = data[smallest], data[i]
         swaps.append((i, smallest))
         heapify(data, n, smallest, swaps)
 
 
-def build_heap(data, n, swaps):
+def build_heap(data, n):
+   
+    swaps = []
     for i in range(n // 2, -1, -1):
         heapify(data, n, i, swaps)
+
+    return swaps
 
 
 def main():
     try:
         n = int(input())
         data = list(map(int, input().split()))
+
         assert len(data) == n
-        swaps = []
-        build_heap(data, n, swaps)
+
+        swaps = build_heap(data, n)
+
         print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
+        for swap in swaps:
+            print(swap[0], swap[1])
+
     except Exception as e:
         print(f"Error: {e}")
-        return
+
 
 if __name__ == "__main__":
     main()
