4f341ea85442913cd0ac82a649c964984ef6a7c2
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-20 01:57:17 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,35 +2,52 @@ def build_heap(data):
     swaps = []
     n = len(data)
     for i in range(n // 2 - 1, -1, -1):
-        min_heapify(data, i, swaps)
+        current, left_child, right_child = i, 2*i+1, 2*i+2
+        while left_child < n:
+            smallest_child = left_child if (right_child >= n or data[left_child] < data[right_child]) else right_child
+            if data[current] > data[smallest_child]:
+                swaps.append((current, smallest_child))
+                data[current], data[smallest_child] = data[smallest_child], data[current]
+                current, left_child, right_child = smallest_child, 2*smallest_child+1, 2*smallest_child+2
+            else:
+                break
     return swaps
 
-def min_heapify(data, i, swaps):
-    n = len(data)
-    smallest = i
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
-
-    if left_child < n and data[left_child] < data[smallest]:
-        smallest = left_child
-
-    if right_child < n and data[right_child] < data[smallest]:
-        smallest = right_child
-
-    if i != smallest:
-        swaps.append((i, smallest))
-        data[i], data[smallest] = data[smallest], data[i]
-        min_heapify(data, smallest, swaps)
 
 def main(): 
-    n = int(input())
-    data = list(map(int, input().split()))
-    assert len(data) == n
+    Input = input() 
+    if "I" in Input:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
 
+        swaps = build_heap(data)
+        print(len(swaps))
+
+    elif "S" in Input:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+
+        for i in range(n):
+            swaps = build_heap(data[:n-i])
+            for swap in swaps:
+                print(swap[0], swap[1])
+            data[0], data[n-i-1] = data[n-i-1], data[0]
+        print(" ".join(map(str, data)))
+        
+    if "F" in Input:  
+        filepath = "tests/" + input() 
+        with open(filepath, 'r') as file:
+            n = int(file.readline().strip())
+            data = list(map(int, file.readline().strip().split()))
+            assert len(data) == n
+            
     swaps = build_heap(data)
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
 
 if __name__ == "__main__":
     main()
