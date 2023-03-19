05e99663c9bec38f40c9810ca61b074da9a07dd5
Ricardsmm221RDB246 <125766591+Ricardsmm221RDB246@users.noreply.github.com>
2023-03-20 00:37:12 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,52 +1,36 @@
-# python3
+def heapify(data, n, i, swaps):
+    smallest = i 
+    left = 2 * i + 1   
+    right = 2 * i + 2     
+    if left < n and data[i] > data[left]:
+        smallest = left
+    if right < n and data[smallest] > data[right]:
+        smallest = right
+    if smallest != i:
+        data[i], data[smallest] = data[smallest], data[i]
+        swaps.append((i, smallest))
+        heapify(data, n, smallest, swaps)
 
 
-def sift_down(data, i, swaps):
-    """Sifts the element down from the given index to its appropriate place."""
-    min_index = i
-    left_child = 2 * i + 1
-    if left_child < len(data) and data[left_child] < data[min_index]:
-        min_index = left_child
-    right_child = 2 * i + 2
-    if right_child < len(data) and data[right_child] < data[min_index]:
-        min_index = right_child
-    if i != min_index:
-        data[i], data[min_index] = data[min_index], data[i]
-        swaps.append((i, min_index))
-        sift_down(data, min_index, swaps)
+def build_heap(data, n, swaps):
+    for i in range(n // 2, -1, -1):
+        heapify(data, n, i, swaps)
 
-def build_heap(data):
-    """Converts the given array to a heap using only 𝑂(𝑛) swaps."""
-    swaps = []
-    for i in range(len(data)//2, -1, -1):
-        sift_down(data, i, swaps)
-    return swaps
 
 def main():
-    # Reading input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # Checking if length of data is the same as the given length
-    assert len(data) == n
-
-    # Calls function to convert the array to heap and returns all swaps
-    swaps = build_heap(data)
-
-    # Output how many swaps were made, which should be less than 4n
-    print(len(swaps))
-
-    # Output all swaps
-    for i, j in swaps:
-        print(i, j)
-
-if __name__ == "__main__":
-    main()```
-
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    try:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        swaps = []
+        build_heap(data, n, swaps)
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
 if __name__ == "__main__":
     main()
+
